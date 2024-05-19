aa95a8a630046a05e15e51b75dc5060566a67d2f
Topo Lefika Rapula <faiththelo@gmail.com>
2024-05-19 09:01:28 +0200

    nav

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const mobileMenu = document.querySelector('.mobileMenu');
+
+    hamburger.addEventListener('click', () => {
+        mobileMenu.classList.toggle('show');
+    });
+});
